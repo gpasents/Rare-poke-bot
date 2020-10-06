9721721871ce09c1a1ec655e0a38ad5e5acfe613
George <gpasents2@gmail.com>
2020-10-07 01:00:17 +0300

    added functionality for fishing

--- index.js
+++ index.js
@@ -10,24 +10,33 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-
-  console.log(msg.author.username);
   if (msg.author.username === 'PokéMeow' && msg.embeds[0] && msg.embeds[0].author.name === 'A wild Pokémon appeared!') {
-    console.log(msg.embeds[0]);
     let footer = msg.embeds[0].footer.text;
     footer = footer.split('\n')[0];
-    if (footer.includes('Rare') || footer.includes('Legendary') || footer.includes('Shiny')) {
+    if (footer.includes('Legendary') || footer.includes('Shiny')) {
       let desc = msg.embeds[0].description;
-      //desc = desc.replace(/:.*:/, '');
       desc = desc.replace(/<.*>/, '');
       const myEmbed = new Discord.MessageEmbed()
         .setColor(msg.embeds[0].color)
         .setImage(msg.embeds[0].image.url)
         .setFooter(footer)
         .setDescription(desc);
-      //msg.channel.send(myEmbed);
       bot.channels.cache.get('761584846836596796').send(myEmbed);
+    }
+  }
+});
 
+bot.on('messageUpdate', async (oldMessage, newMessage) => {
+  if (newMessage.author.username === 'PokéMeow' && newMessage.embeds[0].description.includes('fished out') && newMessage.embeds[0].image.height === 0) {
+    if (newMessage.embeds[0].description.includes('Shiny') || newMessage.embeds[0].description.includes('Kyogre') || newMessage.embeds[0].description.includes('Suicune')) {
+      let desc = newMessage.embeds[0].description;
+      desc = desc.replace(/<.*>/, '');
+      const myEmbed = new Discord.MessageEmbed()
+        .setTitle('A special Pokemon was found in the waters!')
+        .setColor(newMessage.embeds[0].color)
+        .setImage(newMessage.embeds[0].image.url)
+        .setDescription(desc);
+      bot.channels.cache.get('761584846836596796').send(myEmbed);
     }
   }
 });
