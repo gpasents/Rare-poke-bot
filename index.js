28af9343624bbf229b817cc504c20e7d8b27a238
George <gpasents2@gmail.com>
2020-10-10 17:17:54 +0300

    added console

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
+  console.info("1");
   if (msg.author.username === 'PokéMeow' && msg.embeds[0] && msg.embeds[0].author && msg.embeds[0].author.name === 'A wild Pokémon appeared!') {
     let footer = msg.embeds[0].footer.text;
     footer = footer.split('\n')[0];
+    console.info("2");
     if (footer && (footer.includes('Legendary') || footer.includes('Shiny'))) {
+      console.info("3");
       let desc = msg.embeds[0].description;
       desc = desc.replace(/<.*>/, '');
       const myEmbed = new Discord.MessageEmbed()
@@ -31,7 +34,9 @@ bot.on('message', msg => {
 
 bot.on('messageUpdate', async (oldMessage, newMessage) => {
   try {
+    console.info("4");
     if (newMessage.author.username === 'PokéMeow' && newMessage.embeds[0] && newMessage.embeds[0].description && newMessage.embeds[0].description.includes('fished out') && newMessage.embeds[0].image.height === 0) {
+      console.info("5");
       if (newMessage.embeds[0].description.includes('Shiny') || newMessage.embeds[0].description.includes('Kyogre') || newMessage.embeds[0].description.includes('Suicune')) {
         let desc = newMessage.embeds[0].description;
         desc = desc.replace(/<.*>/, '');
