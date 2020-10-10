d68ebd3f7566112fbfbbd77b555e4f74a18f2b0a
George <gpasents2@gmail.com>
2020-10-10 17:21:16 +0300

    changed logs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  console.info("1");
+  //console.info("1");
   if (msg.author.username === 'PokéMeow' && msg.embeds[0] && msg.embeds[0].author && msg.embeds[0].author.name === 'A wild Pokémon appeared!') {
     let footer = msg.embeds[0].footer.text;
     footer = footer.split('\n')[0];
@@ -34,7 +34,7 @@ bot.on('message', msg => {
 
 bot.on('messageUpdate', async (oldMessage, newMessage) => {
   try {
-    console.info("4");
+    //console.info("4");
     if (newMessage.author.username === 'PokéMeow' && newMessage.embeds[0] && newMessage.embeds[0].description && newMessage.embeds[0].description.includes('fished out') && newMessage.embeds[0].image.height === 0) {
       console.info("5");
       if (newMessage.embeds[0].description.includes('Shiny') || newMessage.embeds[0].description.includes('Kyogre') || newMessage.embeds[0].description.includes('Suicune')) {
