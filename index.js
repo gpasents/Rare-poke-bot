891cc2b9f9a1c1e45f046c9db06f6708091eeb0e
Giorgos Pasentsis <gpasents@gmail.com>
2020-10-15 21:15:32 +0300

    added experiments

--- index.js
+++ index.js
@@ -10,12 +10,43 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
+  if (msg.author.username === 'PokéMeow' &&  msg.embeds[0] &&  msg.embeds[0].title &&  msg.embeds[0].title.includes("hatched")){
+    console.info("CONTENT :" + msg.content);
+    console.info("embed :" + JSON.stringify(msg.embeds[0]));
+    // let title = msg.embeds[0].title;
+    // title = title.replace(/<.*>/, '');
+    // const myEmbed = new Discord.MessageEmbed()
+    // .setColor(msg.embeds[0].color)
+    // .setImage(msg.embeds[0].image.url)
+    // .setDescription(title);
+    // bot.channels.cache.get('764227765234565130').send(myEmbed);
+  }
+  if (msg.author.username === 'PokéMeow' &&  msg.embeds[0] &&  msg.embeds[0].description &&  msg.embeds[0].description.includes("from a swap") &&  (msg.embeds[0].description.includes("Shiny") || msg.embeds[0].description.includes("Legendary") || msg.embeds[0].description.includes("Golden"))){
+    let desc = msg.embeds[0].description;
+    desc = desc.replace(/<.*>/, '');
+    const myEmbed = new Discord.MessageEmbed()
+    .setColor(msg.embeds[0].color)
+    .setImage(msg.embeds[0].image.url)
+    .setDescription(desc);
+    console.info("embed :" + JSON.stringify(msg.embeds[0]));
+    bot.channels.cache.get('764227765234565130').send(myEmbed);
+  }
   if (msg.author.username === 'PokéMeow' && msg.embeds[0] && msg.embeds[0].author.name === 'A wild Pokémon appeared!') {
     let footer = msg.embeds[0].footer.text;
     footer = footer.split('\n')[0];
-    if (footer.includes('Legendary') || footer.includes('Shiny')) {
+    if (footer.includes('Golden') || footer.includes('Legendary') || footer.includes('Shiny')) {
       let desc = msg.embeds[0].description;
-      desc = desc.replace(/<.*>/, '');
+      if (footer.includes('Shiny')) {
+        desc = desc.substr(desc.indexOf(" ") + 1);
+        desc = desc.replace(/<.*>/, '');
+        console.info("my Desc shiny : " + desc);
+      } else {
+        desc = desc.replace(/<.*>/, '');
+        console.info("my Desc legend : " + desc);
+        if (footer.includes('Legendary')) {
+          console.info("THE LEGENDARY COLOR: " + msg.embeds[0].color);
+        }
+      }
       const myEmbed = new Discord.MessageEmbed()
         .setColor(msg.embeds[0].color)
         .setImage(msg.embeds[0].image.url)
@@ -31,7 +62,7 @@ bot.on('message', msg => {
 
 bot.on('messageUpdate', async (oldMessage, newMessage) => {
   if (newMessage.author.username === 'PokéMeow' && newMessage.embeds[0].description.includes('fished out') && newMessage.embeds[0].image.height === 0) {
-    if (newMessage.embeds[0].description.includes('Shiny') || newMessage.embeds[0].description.includes('Kyogre') || newMessage.embeds[0].description.includes('Suicune')) {
+    if (newMessage.embeds[0].title.includes('special') || newMessage.embeds[0].description.includes('Shiny') || newMessage.embeds[0].description.includes('Kyogre') || newMessage.embeds[0].description.includes('Suicune')) {
       let desc = newMessage.embeds[0].description;
       desc = desc.replace(/<.*>/, '');
       const myEmbed = new Discord.MessageEmbed()
